e5b6dbf94689791bc887f93d5e54a20b3596a804
jakhongirrasulov007 <jakhongirrasulov001@gmail.com>
2021-05-23 21:04:36 +0500

    main changes are made closes #3

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::resource('posts', PostsController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
