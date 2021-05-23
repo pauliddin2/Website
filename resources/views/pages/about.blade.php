442292ccca6c80a4f0ecb21e0be4472891fabd53
Shohruh-Asatov <72484599+Shohruh-Asatov@users.noreply.github.com>
2021-05-23 23:24:33 +0500

    Add files via upload

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,26 @@
+  @extends('layouts.app')
+  @section('content')
+  <header class="masthead" style="background-image:url('assets/img/about-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="site-heading">
+                        <h1>About Me</h1><h5><span class="subheading">About our University</span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <p>One of the most crucial factors determining the success and self-sufficiency of an individual, in today's competitive environment, driven by the achievements of science and technology, is Education.
+
+                    We know this, and thus, our high-qualified faculty takes the full responsibility on providing you with all the necessary knowledge and skills to succeed in the 21st century. Our foremost goal is to become a part of your future success and share the joy of your great achievements!
+
+                    Your future begins today!</p>
+                </div>
+        </div>
+    </div>
+@endsection
