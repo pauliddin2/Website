88052c8c36b3b25c55be980921379e1181edb6ad
Shohruh-Asatov <72484599+Shohruh-Asatov@users.noreply.github.com>
2021-05-23 23:26:35 +0500

    Add files via upload

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,21 @@
+<footer style="color: var(--dark);background: var(--white);">
+    <div class="container" id="footerinfo">
+        <div class="row" style="height: 102px;">
+            <div class="col">
+                <p class="text-left"
+                    style="margin-top: 28px;font-family: 'Open Sans', sans-serif;font-weight: 300;font-size: 14px;">
+                    TecnoNews, Taskent, Uzbekistan<br /></p>
+            </div>
+            <div class="col"><a href="https://mx.linkedin.com/" target="_blank"><i class="fa fa-linkedin"
+                        style="font-size: 22px;letter-spacing: 13px;"></i></a><a href="" target="_blank"><i
+                        class="fa fa-youtube-play" style="font-size: 22px;padding-left: 10px;"></i></a>
+
+            </div>
+            <div class="row">
+                <p class="text-right" style="margin-top: 28px;font-size: 14px;font-weight: 300;">The Inha University in Tashkent<br /></p>
+                <p class="text-right mt-4" ><a href="https://inha.uz/"><img id="ignicionlogo"
+                            src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Emblem-IUT.jpg" width="80px" /></a></p>
+            </div>
+        </div>
+    </div>
+</footer>
